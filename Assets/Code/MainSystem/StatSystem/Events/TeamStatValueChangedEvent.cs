2cef248fc5a04d30f6bd5cc7db4a2641f21d74a3
kimjinho <russiakim2008@gmail.com>
2025-12-07 22:54:24 +0900

    feat : team stat value changed event

---
+++ Assets/Code/MainSystem/StatSystem/Events/TeamStatValueChangedEvent.cs
@@ -0,0 +1,17 @@
+﻿using Code.Core.Bus;
+using Code.MainSystem.StatSystem.BaseStats;
+
+namespace Code.MainSystem.StatSystem.Events
+{
+    public struct TeamStatValueChangedEvent : IEvent
+    {
+        public StatType StatType;
+        public int AddValue;
+
+        public TeamStatValueChangedEvent(StatType statType, int addValue)
+        {
+            StatType = statType;
+            AddValue = addValue;
+        }
+    }
+}
