2dbd2394674f9f0a841aee92ae785520014033f8
kimjinho <russiakim2008@gmail.com>
2025-12-07 17:56:57 +0900

    feat : member stat

---
+++ Assets/Code/MainSystem/StatSystem/MemberStats/MemberStat.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Code.Core.Bus;
+using UnityEngine;
+using Code.MainSystem.StatSystem.BaseStats;
+using Code.MainSystem.StatSystem.Events;
+
+namespace Code.MainSystem.StatSystem.MemberStats
+{
+    public class MemberStat : AbstractStats
+    {
+        [SerializeField] protected List<StatData> memberStatData;
+        
+        protected readonly Dictionary<StatType, BaseStat> MemberStats = new();
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            foreach (var data in memberStatData)
+            {
+                BaseStat stat = new BaseStat(data);
+                MemberStats[data.statType] = stat;
+            }
+        }
+        
+        public void MemberStatUpgrade(StatType statType, float failureValue)
+        {
+            float randValue = Random.Range(0f, 101f);
+            if (randValue <= failureValue)
+            {
+                Bus<StatUpgradeEvent>.Raise(new StatUpgradeEvent(false));
+            }
+            else
+            {
+                BaseStat stat = MemberStats.GetValueOrDefault(statType);
+                if (stat != null)
+                {
+                    stat.PlusValue(100);
+                    Bus<StatUpgradeEvent>.Raise(new StatUpgradeEvent(true));
+                }
+            }
+        }
+
+        public BaseStat GetMemberStat(StatType statType)
+        {
+            return MemberStats.GetValueOrDefault(statType);  
+        }
+
+        public BaseStat GetStat(StatType statType)
+        {
+            return MemberStats.GetValueOrDefault(statType) 
+                   ?? CommonStats.GetValueOrDefault(statType);
+        }
+    }
+}
