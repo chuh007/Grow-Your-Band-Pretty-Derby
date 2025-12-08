228b8f3f5b58781a83fe00e92d8c144ffa574d34
dlckdgh0414 <ci5976547@gmail.com>
2025-12-08 17:41:35 +0900

    feat : MemberData

---
+++ Assets/Code/MainSystem/MainScreen/MemberData/UnitStatSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Code.MainSystem.StatSystem.BaseStats;
+using UnityEngine;
+
+namespace Code.MainSystem.MainScreen.MemberData
+{
+    [CreateAssetMenu(fileName = "Unit", menuName = "SO/Unit/Data", order = 0)]
+    public class UnitStatSO : ScriptableObject
+    {
+        public string unitName;
+        public Sprite unitImage;
+        public List<StatData> stats;
+        public int currentHealth;
+        public int maxHealth;
+    }
+}
