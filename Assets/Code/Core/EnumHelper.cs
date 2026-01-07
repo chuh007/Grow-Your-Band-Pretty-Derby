b9bb8c1a9e9cc6e7aae4e54840d56af950e5ec95
dlckdgh0414 <ci5976547@gmail.com>
2026-01-07 18:51:29 +0900

    fix : BottomBar

--- Assets/Code/Core/EnumHelper.cs
+++ Assets/Code/Core/EnumHelper.cs
@@ -5,4 +5,15 @@
         Personal,
         Team
     }
+
+    public enum BottomTabMode
+    {
+        None = 0,
+        Mode1 = 1,
+        Mode2 = 2,
+        Mode3 = 3,
+        Mode4 = 4,
+        Mode5 = 5,
+        Mode6 = 6,
+    }
 }
