7e4f9080047f4d6335cf7515836285a43b2e4491
kimjinho <russiakim2008@gmail.com>
2026-01-10 18:11:50 +0900

    test : test condition, effect

---
+++ Assets/Code/MainSystem/TraitSystem/TraitConditions/TestCondition.cs
@@ -0,0 +1,13 @@
+﻿using Code.MainSystem.TraitSystem.Contexts;
+using Code.MainSystem.TraitSystem.Interface;
+
+namespace Code.MainSystem.TraitSystem.TraitConditions
+{
+    public class TestCondition : ITraitCondition
+    {
+        public bool IsMet(GameContext context)
+        {
+            return true;
+        }
+    }
+}
