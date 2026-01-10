df3f7e0c50354bb64084815d7719f452c0ab7497
kimjinho <russiakim2008@gmail.com>
2026-01-10 17:44:55 +0900

    feat : trait basic interface

--- Assets/Code/MainSystem/TraitSystem/Contexts/GameContext.cs
+++ Assets/Code/MainSystem/TraitSystem/Contexts/GameContext.cs
@@ -1,7 +1,14 @@
-﻿namespace Code.MainSystem.TraitSystem.Contexts
+﻿using Code.MainSystem.TraitSystem.Data;
+
+namespace Code.MainSystem.TraitSystem.Contexts
 {
+    /// <summary>
+    /// 특성 판정에 필요한 모든 정보를 담아서 전달하는 객체
+    /// </summary>
     public class GameContext
     {
-        
+        public CharacterTrait Owner;
+        public int CurrentTurn;
+        //필요시 다른 정보 추가 해야함
     }
 }
