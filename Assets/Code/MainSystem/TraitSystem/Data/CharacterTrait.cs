06d83bbfc1758647579a7f88b9d0643774d6c5de
kimjinho <russiakim2008@gmail.com>
2026-01-10 15:50:15 +0900

    feat : basic character traits

---
+++ Assets/Code/MainSystem/TraitSystem/Data/CharacterTrait.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.MainSystem.TraitSystem.Data
+{
+    public class CharacterTrait : MonoBehaviour
+    {
+        [SerializeField] private List<TraitDataSO> traitData;
+        [SerializeField] private int currentLevel;
+        
+        public List<TraitDataSO> TraitData => traitData;
+        public int CurrentLevel => currentLevel;
+    }
+}
