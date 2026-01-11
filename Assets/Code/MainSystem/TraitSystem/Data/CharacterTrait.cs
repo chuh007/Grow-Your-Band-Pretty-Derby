9d316877e3ca222dee40a5088a1a7ab2c85c21a9
kimjinho <russiakim2008@gmail.com>
2026-01-11 19:00:29 +0900

    fix : character trait

--- Assets/Code/MainSystem/TraitSystem/Data/CharacterTrait.cs
+++ Assets/Code/MainSystem/TraitSystem/Data/CharacterTrait.cs
@@ -1,28 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using UnityEngine;
+using System.Collections.Generic;
 using Code.MainSystem.TraitSystem.Interface;
 using Code.MainSystem.TraitSystem.Runtime;
-using UnityEngine;
 
 namespace Code.MainSystem.TraitSystem.Data
 {
     public class CharacterTrait : MonoBehaviour, ITraitHolder
     {
-        [SerializeField] private List<TraitDataSO> startingTraits;
-        
         private List<ActiveTrait> _activeTraits = new List<ActiveTrait>();
         
         public IReadOnlyList<ActiveTrait> ActiveTraits => _activeTraits;
-
-        private void Awake()
-        {
-            InitializeTraits();
-        }
-
-        private void InitializeTraits()
-        {
-            foreach (var data in startingTraits)
-                AddTrait(data);
-        }
 
         public void AddTrait(TraitDataSO data)
         {
