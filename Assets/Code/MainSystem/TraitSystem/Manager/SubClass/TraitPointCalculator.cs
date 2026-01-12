0a558cfb9582406d9cd9be628c7d5533dbd0cb9a
kimjinho <russiakim2008@gmail.com>
2026-01-12 20:30:23 +0900

    feat : trait system installer

--- Assets/Code/MainSystem/TraitSystem/Manager/SubClass/TraitPointCalculator.cs
+++ Assets/Code/MainSystem/TraitSystem/Manager/SubClass/TraitPointCalculator.cs
@@ -1,22 +1,19 @@
 ﻿using System.Linq;
 using Code.MainSystem.TraitSystem.Data;
 using Code.MainSystem.TraitSystem.Interface;
-using UnityEngine;
 
 namespace Code.MainSystem.TraitSystem.Manager.SubClass
 {
     /// <summary>
     /// 포인트 처리 담당
     /// </summary>
-    public class TraitPointCalculator : MonoBehaviour, ITraitPointCalculator
+    public class TraitPointCalculator : ITraitPointCalculator
     {
         public int CalculateTotalPoint(ITraitHolder holder)
         {
             if (holder is CharacterTrait character)
-            {
-                // ActiveTraits에 담긴 데이터의 Point 합산 (양수/음수 모두 포함)
                 return character.ActiveTraits.Sum(t => t.Data.Point);
-            }
+            
             return 0;
         }
     }
---
+++ Assets/Code/MainSystem/TraitSystem/Manager/SubClass/TraitSystemInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Code.MainSystem.TraitSystem.Interface;
+using Reflex.Core;
+using UnityEngine;
+
+namespace Code.MainSystem.TraitSystem.Manager.SubClass
+{
+    public class TraitSystemInstaller : MonoBehaviour, IInstaller
+    {
+        public void InstallBindings(ContainerBuilder builder)
+        {
+            builder.AddSingleton(typeof(TraitPointCalculator), typeof(ITraitPointCalculator));
+        }
+    }
+}
