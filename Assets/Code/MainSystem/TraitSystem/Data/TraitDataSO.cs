56fef0060d4249da2b380eb9c6a48eaa3ce77b03
kimjinho <russiakim2008@gmail.com>
2026-01-12 20:57:15 +0900

    feat : trait type in trait data so

--- Assets/Code/MainSystem/TraitSystem/Data/TraitDataSO.cs
+++ Assets/Code/MainSystem/TraitSystem/Data/TraitDataSO.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using UnityEngine;
+using System.Collections.Generic;
 using Code.MainSystem.TraitSystem.Interface;
-using UnityEngine;
 
 namespace Code.MainSystem.TraitSystem.Data
 {
-    public enum TraitType
+    public enum TraitEffectType
     {
         Training, Performance, Etc
     }
@@ -13,14 +13,29 @@ namespace Code.MainSystem.TraitSystem.Data
     {
         None, TurnBased, EventBased, ConditionBased
     }
+
+    public enum TraitType
+    {
+        None,                   // 특성 없음
+        Telepathy,              // 이심전심
+        LoneGuitarist,          // 고독한 기타리스트
+        ShiningEyes,            // 반짝이는 눈
+        FailureBreedsSuccess,   // 실패는 성공의 어머니
+        HonedTechnique,         // 단련기술
+        Injury,                 // 부상
+        Overzealous,            // 지나친 열정
+        Dogmatic,               // 독선적
+        Entertainer             // 만담가
+    }
     
     [CreateAssetMenu(fileName = "Trait data", menuName = "SO/Trait/Trait data", order = 0)]
     public class TraitDataSO : ScriptableObject
     {
         public int TraitID;
+        public TraitType TraitType;
         public string TraitName;
 
-        public TraitType TraitType;
+        public TraitEffectType traitEffectType;
 
         public int Level;
         public int Point;
