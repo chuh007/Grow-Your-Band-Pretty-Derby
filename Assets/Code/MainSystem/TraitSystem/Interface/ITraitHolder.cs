09e1c8579f793df834a57ede02a06cbfc3b20883
kimjinho <russiakim2008@gmail.com>
2026-01-12 20:57:41 +0900

    fix : trait holder

--- Assets/Code/MainSystem/TraitSystem/Interface/ITraitHolder.cs
+++ Assets/Code/MainSystem/TraitSystem/Interface/ITraitHolder.cs
@@ -1,4 +1,5 @@
-﻿using Code.MainSystem.TraitSystem.Data;
+﻿using System.Collections.Generic;
+using Code.MainSystem.TraitSystem.Data;
 using Code.MainSystem.TraitSystem.Runtime;
 
 namespace Code.MainSystem.TraitSystem.Interface
@@ -6,6 +7,7 @@ namespace Code.MainSystem.TraitSystem.Interface
     public interface ITraitHolder
     {
         int MaxPoints { get; }
+        IReadOnlyList<ActiveTrait> ActiveTraits { get; }
         void AddTrait(TraitDataSO newTrait);
         void RemoveActiveTrait(ActiveTrait trait);
     }
