272fba022411363fff74e150a70869a9dddaed16
chuh007 <chhuh007@gmail.com>
2026-01-13 23:20:24 +0900

    feat : Menu. fix : DayChange

---
+++ Assets/Code/MainSystem/Turn/UI/DayOfWeekUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Code.MainSystem.Turn.UI
+{
+    public class DayOfWeekUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI dayText;
+        [SerializeField] private TextMeshProUGUI dayOfWeekText;
+        
+        public RectTransform RectTransform { get; private set; }
+        
+        private void Awake()
+        {
+            RectTransform = GetComponent<RectTransform>();
+        }
+
+        public void SetDayText(int dayCount) => dayText.SetText(dayCount.ToString());
+        public void SetDayOfWeekText(string dayOfWeek) => dayOfWeekText.SetText(dayOfWeek);
+    }
+}
