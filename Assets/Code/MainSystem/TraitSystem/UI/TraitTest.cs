af64694e0f262b3dbc03028b84cf25fae1f3f853
kimjinho <russiakim2008@gmail.com>
2026-01-15 19:55:22 +0900

    feat : trait controller ui

---
+++ Assets/Code/MainSystem/TraitSystem/UI/TraitTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Code.Core.Bus;
+using Code.MainSystem.StatSystem.Manager;
+using Code.Core.Bus.GameEvents.TraitEvents;
+using Code.MainSystem.TraitSystem.Data;
+using Code.MainSystem.TraitSystem.Manager;
+using Reflex.Attributes;
+using TMPro;
+
+namespace Code.MainSystem.TraitSystem.UI
+{
+    public class TraitTest : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI label;
+        [Inject] TraitManager _manager;
+        [SerializeField] private MemberType memberType;
+        [SerializeField] private TraitType traitType;
+        
+        public void ShowList()
+        {
+            label.SetText($"{memberType} 특성 UI");
+            Bus<TraitShowRequested>.Raise(new TraitShowRequested(memberType));
+        }
+
+        public void AddTrait()
+        {
+            Bus<TraitAddRequested>.Raise(new TraitAddRequested(_manager.CurrentMember, traitType));
+            Bus<TraitShowRequested>.Raise(new TraitShowRequested(_manager.CurrentMember));
+        }
+    }
+}
