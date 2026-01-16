8ac97ba24e1d4251389d9115a6be28aebc5fab91
kimjinho <russiakim2008@gmail.com>
2026-01-16 17:13:16 +0900

    feat : trait overlay cancel

--- Assets/Code/MainSystem/TraitSystem/UI/TraitOverlayUI.cs
+++ Assets/Code/MainSystem/TraitSystem/UI/TraitOverlayUI.cs
@@ -1,42 +1,68 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace Code.MainSystem.TraitSystem.UI
 {
     public class TraitOverlayUI : MonoBehaviour
     {
         [SerializeField] private CanvasGroup overlayCanvasGroup;
-        private int _panelCount;
+        private readonly Stack<GameObject> _panelStack = new Stack<GameObject>();
         private const float AlphaPerPanel = 0.3f;
 
         private void Awake()
         {
-            _panelCount = 0;
             overlayCanvasGroup.alpha = 0;
             overlayCanvasGroup.gameObject.SetActive(false);
         }
 
-        public void OnPanelOpened()
+        public void OnPanelOpened(GameObject panel)
         {
-            Debug.Log("OnPanelOpened");
-            _panelCount++;
+            _panelStack.Push(panel);
             UpdateOverlayAlpha();
         }
     
-        public void OnPanelClosed()
+        public void OnPanelClosed(GameObject panel)
         {
-            if (_panelCount > 0)
-                _panelCount--;
+            if (_panelStack.Count > 0 && _panelStack.Peek() == panel)
+            {
+                _panelStack.Pop();
+            }
+            else
+            {
+                var temp = new Stack<GameObject>();
+                while (_panelStack.Count > 0)
+                {
+                    var p = _panelStack.Pop();
+                    if (p != panel)
+                        temp.Push(p);
+                }
+                while (temp.Count > 0)
+                    _panelStack.Push(temp.Pop());
+            }
     
             UpdateOverlayAlpha();
     
-            if (_panelCount == 0)
+            if (_panelStack.Count == 0)
+                overlayCanvasGroup.gameObject.SetActive(false);
+        }
+        
+        public void OnOverlayClicked()
+        {
+            if(_panelStack.Count <= 0)
+                return;
+            
+            GameObject topPanel = _panelStack.Pop();
+            topPanel.SetActive(false);
+            
+            UpdateOverlayAlpha();
+            
+            if (_panelStack.Count == 0)
                 overlayCanvasGroup.gameObject.SetActive(false);
         }
     
         private void UpdateOverlayAlpha()
         {
-            float newAlpha = Mathf.Min(_panelCount * AlphaPerPanel, 0.8f);
-            Debug.Log($"Panel Count: {_panelCount}, New Alpha: {newAlpha}");
+            float newAlpha = Mathf.Min(_panelStack.Count * AlphaPerPanel, 0.8f);
     
             overlayCanvasGroup.alpha = newAlpha;
             overlayCanvasGroup.gameObject.SetActive(true);
