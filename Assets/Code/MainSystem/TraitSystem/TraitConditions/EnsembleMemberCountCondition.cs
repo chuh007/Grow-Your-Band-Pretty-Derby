d1ca126fd21e2273906fd2f056e620ba0f440205
kimjinho <russiakim2008@gmail.com>
2026-01-17 18:30:04 +0900

    feat : temporary effect, condition

---
+++ Assets/Code/MainSystem/TraitSystem/TraitConditions/EnsembleMemberCountCondition.cs
@@ -0,0 +1,13 @@
+﻿using Code.MainSystem.TraitSystem.Interface;
+using Code.MainSystem.TraitSystem.Runtime;
+
+namespace Code.MainSystem.TraitSystem.TraitConditions
+{
+    public class EnsembleMemberCountCondition : ITraitCondition
+    {
+        public bool IsMet(ITraitHolder holder, ActiveTrait trait)
+        {
+            return true;
+        }
+    }
+}
