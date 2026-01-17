bd95272bb010246ca25ab185c97913c8e2b35740
kimjinho <russiakim2008@gmail.com>
2026-01-17 18:27:41 +0900

    feat : TraitInteraction system

---
+++ Assets/Code/MainSystem/TraitSystem/Interface/ITraitInteraction.cs
@@ -0,0 +1,10 @@
+﻿using Code.MainSystem.TraitSystem.Runtime;
+
+namespace Code.MainSystem.TraitSystem.Interface
+{
+    public interface ITraitInteraction
+    {
+        bool CanInteract(ActiveTrait traitA, ActiveTrait traitB);
+        void ProcessInteraction(ITraitHolder holder, ActiveTrait traitA, ActiveTrait traitB);
+    }
+}
