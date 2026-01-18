55ce4840cdd316947e128dce6180c90cc18620f2
kimjinho <russiakim2008@gmail.com>
2026-01-18 18:27:51 +0900

    feat : abstract trait effect and condition

---
+++ Assets/Code/MainSystem/TraitSystem/TraitConditions/TraitCondition.cs
@@ -0,0 +1,23 @@
+﻿using Code.MainSystem.TraitSystem.Runtime;
+using Code.MainSystem.TraitSystem.Interface;
+
+namespace Code.MainSystem.TraitSystem.TraitConditions
+{
+    /// <summary>
+    /// TraitCondition 추상 베이스 클래스
+    /// </summary>
+    public abstract class TraitCondition : ITraitCondition
+    {
+        public bool IsMet(ITraitHolder holder, ActiveTrait trait)
+        {
+            var result = CheckCondition(holder, trait);
+            
+            return result;
+        }
+        
+        /// <summary>
+        /// 실제 조건 체크 로직
+        /// </summary>
+        protected abstract bool CheckCondition(ITraitHolder holder, ActiveTrait trait);
+    }
+}
