78348fac3c1d3aea6db3191613c5f7f03939d6b4
kimjinho <russiakim2008@gmail.com>
2026-01-18 20:11:51 +0900

    fix : trait data

--- Assets/Code/MainSystem/TraitSystem/Data/TraitDataSO.cs
+++ Assets/Code/MainSystem/TraitSystem/Data/TraitDataSO.cs
@@ -1,13 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections.Generic;
-using Code.MainSystem.TraitSystem.Interface;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using Code.MainSystem.TraitSystem.TraitConditions;
 using Code.MainSystem.TraitSystem.TraitEffect;
-using UnityEngine.Serialization;
 
 namespace Code.MainSystem.TraitSystem.Data
 {
-    // 효과 적용 시점 정의
     public enum TraitEffectType
     {
         OnTurnStart,
@@ -15,17 +16,20 @@ namespace Code.MainSystem.TraitSystem.Data
         OnActionExecute,
         OnActionComplete,
         OnTurnEnd,
-        Passive // 항상 적용
+        Passive
     }
-    
+
     public enum ConditionType
     {
-        NoneCondition, TurnBased, EventBased, ConditionBased
+        NoneCondition,
+        TurnBased,
+        EventBased,
+        ConditionBased
     }
 
     public enum TraitType
     {
-        NoneTrait,                   // 특성 없음
+        NoneTrait,              // 특성 없음
         Telepathy,              // 이심전심
         LoneGuitarist,          // 고독한 기타리스트
         ShiningEyes,            // 반짝이는 눈
@@ -36,7 +40,7 @@ namespace Code.MainSystem.TraitSystem.Data
         Dogmatic,               // 독선적
         Entertainer             // 만담가
     }
-    
+
     [CreateAssetMenu(fileName = "Trait data", menuName = "SO/Trait/Trait data", order = 0)]
     public class TraitDataSO : ScriptableObject
     {
@@ -44,7 +48,7 @@ namespace Code.MainSystem.TraitSystem.Data
         public TraitType TraitType;
         public string TraitName;
         public Sprite TraitIcon;
-        
+
         public TraitEffectType traitEffectType;
 
         public int Level;
@@ -55,10 +59,87 @@ namespace Code.MainSystem.TraitSystem.Data
         public ConditionType conditionType;
 
         public List<float> Effects;
-        
         [TextArea] public string DescriptionEffect;
-        
-        [SerializeReference] public ITraitCondition Condition;
-        [SerializeReference] public ITraitEffect Effect;
+
+        public BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+        [HideInInspector] public string conditionName;
+        [HideInInspector] public string effectName;
+
+        [HideInInspector] public string conditionTypeName;
+        [HideInInspector] public string effectTypeName;
+
+        private Action<AbstractTraitCondition> _traitConditionSetter;
+        private Action<AbstractTraitEffect> _traitEffectSetter;
+
+        public bool InitializeTrait()
+        {
+            return ConditionFactory() && EffectFactory();
+        }
+
+        public void ApplyCondition(AbstractTraitCondition condition)
+        {
+            _traitConditionSetter?.Invoke(condition);
+        }
+
+        public void ApplyEffect(AbstractTraitEffect effect)
+        {
+            _traitEffectSetter?.Invoke(effect);
+        }
+
+        private bool ConditionFactory()
+        {
+            var parentType = typeof(AbstractTraitCondition);
+            var traitType = FindType(conditionName);
+            if (traitType == null || !parentType.IsAssignableFrom(traitType))
+                return false;
+
+            var targetField = traitType.GetField(conditionTypeName, bindingFlags);
+            if (targetField == null || targetField.FieldType != typeof(ConditionType))
+                return false;
+
+            ParameterExpression param = Expression.Parameter(parentType, "condition");
+            UnaryExpression casted = Expression.Convert(param, traitType);
+            MemberExpression fieldAccess = Expression.Field(casted, targetField);
+            ConstantExpression value = Expression.Constant(conditionType);
+            BinaryExpression assign = Expression.Assign(fieldAccess, value);
+
+            _traitConditionSetter = Expression.Lambda<Action<AbstractTraitCondition>>(assign, param).Compile();
+
+            return true;
+        }
+
+        private bool EffectFactory()
+        {
+            var parentType = typeof(AbstractTraitEffect);
+            var traitType = FindType(effectName);
+            if (traitType == null || !parentType.IsAssignableFrom(traitType))
+                return false;
+
+            var targetField = traitType.GetField(effectTypeName, bindingFlags);
+            if (targetField == null || targetField.FieldType != typeof(TraitEffectType))
+                return false;
+
+            ParameterExpression param = Expression.Parameter(parentType, "effect");
+            UnaryExpression casted = Expression.Convert(param, traitType);
+            MemberExpression fieldAccess = Expression.Field(casted, targetField);
+            ConstantExpression value = Expression.Constant(traitEffectType);
+            BinaryExpression assign = Expression.Assign(fieldAccess, value);
+
+            _traitEffectSetter = Expression.Lambda<Action<AbstractTraitEffect>>(assign, param).Compile();
+
+            return true;
+        }
+
+        private static Type FindType(string fullName)
+        {
+            if (string.IsNullOrEmpty(fullName))
+                return null;
+
+            return AppDomain.CurrentDomain
+                .GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .FirstOrDefault(t => t.FullName == fullName);
+        }
     }
 }
