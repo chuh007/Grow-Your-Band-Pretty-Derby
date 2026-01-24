a0660066a70821e5a253eb5b9422ce61c12718b0
kimjinho <russiakim2008@gmail.com>
2026-01-24 14:57:17 +0900

    feat : trait group data

---
+++ Assets/Code/MainSystem/TraitSystem/Data/TraitGroupDataSO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Code.MainSystem.TraitSystem.Data
+{
+    public enum TraitGroupType
+    {
+        None,
+        Good
+    }
+    
+    [CreateAssetMenu(fileName = "Trait group", menuName = "SO/Trait/Trait group", order = 0)]
+    public class TraitGroupDataSO : ScriptableObject
+    {
+        public  TraitGroupType GroupType;
+        public string GroupName;
+        public List<TraitDataSO> TraitData;
+    }
+}
