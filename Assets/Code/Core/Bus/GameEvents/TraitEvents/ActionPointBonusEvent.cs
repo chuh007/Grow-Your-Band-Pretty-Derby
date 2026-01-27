a508b72612567084192e8f35c957a1f249ad781a
kimjinho <russiakim2008@gmail.com>
2026-01-27 15:59:47 +0900

    refactor : trait management system

---
+++ Assets/Code/Core/Bus/GameEvents/TraitEvents/ActionPointBonusEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Code.Core.Bus.GameEvents.TraitEvents
+{
+    public struct ActionPointBonusEvent : IEvent
+    {
+        public int BonusAmount { get; }
+        
+        public ActionPointBonusEvent(int bonusAmount)
+        {
+            BonusAmount = bonusAmount;
+        }
+    }
+}
