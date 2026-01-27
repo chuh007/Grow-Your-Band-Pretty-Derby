5581cbbd9c11e857116e14dc8d5f244d61403648
kimjinho <russiakim2008@gmail.com>
2026-01-27 20:15:49 +0900

    feat : modifier interfaces

---
+++ Assets/Code/MainSystem/TraitSystem/Interface/IEtcStats.cs
@@ -0,0 +1,22 @@
+﻿namespace Code.MainSystem.TraitSystem.Interface
+{
+    // 행동권 추가 (반짝이는 눈, 지나친 열정)
+    public interface IActionPointBonus
+    {
+        float Chance { get; } // N1%
+        int Amount { get; }   // N2
+    }
+
+    // 영감 시스템 (실패는 성공의 어머니)
+    public interface IInspirationSystem
+    {
+        int GainOnFailure { get; }
+        int MaxInspiration { get; }
+    }
+
+    // 판정 보정 (집중력)
+    public interface IJudgmentCorrection
+    {
+        bool CorrectMissToGood { get; }
+    }
+}
