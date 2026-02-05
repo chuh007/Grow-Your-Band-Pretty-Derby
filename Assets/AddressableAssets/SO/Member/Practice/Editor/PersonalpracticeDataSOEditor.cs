9fe9f1244733fdd618e8e87ae6c80e94c7e63a44
dlckdgh0414 <ci5976547@gmail.com>
2026-02-05 13:19:43 +0900

    feat : PracticeEditor

---
+++ Assets/AddressableAssets/SO/Member/Practice/Editor/PersonalpracticeDataSOEditor.cs
@@ -0,0 +1,138 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using Code.MainSystem.StatSystem.BaseStats;
+
+namespace Code.MainSystem.MainScreen.MemberData
+{
+    [CustomEditor(typeof(PersonalpracticeDataSO))]
+    public class PersonalpracticeDataSOEditor : Editor
+    {
+        private SerializedProperty practiceStatType;
+        private SerializedProperty practiceStatName;
+        private SerializedProperty personalpracticeDescription;
+        private SerializedProperty staminaReduction;
+        private SerializedProperty statIncrease;
+        private SerializedProperty personalsuccessComment;
+        private SerializedProperty personalfaillComment;
+
+        private bool showSuccessComment = true;
+        private bool showFailComment = true;
+
+        private void OnEnable()
+        {
+            practiceStatType = serializedObject.FindProperty("PracticeStatType");
+            practiceStatName = serializedObject.FindProperty("PracticeStatName");
+            personalpracticeDescription = serializedObject.FindProperty("PersonalpracticeDescription");
+            staminaReduction = serializedObject.FindProperty("StaminaReduction");
+            statIncrease = serializedObject.FindProperty("statIncrease");
+            personalsuccessComment = serializedObject.FindProperty("PersonalsuccessComment");
+            personalfaillComment = serializedObject.FindProperty("PersonalfaillComment");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            PersonalpracticeDataSO data = (PersonalpracticeDataSO)target;
+            
+            GUIStyle headerStyle = new GUIStyle(GUI.skin.label)
+            {
+                fontSize = 14,
+                fontStyle = FontStyle.Bold,
+                alignment = TextAnchor.MiddleCenter
+            };
+
+            GUIStyle boxStyle = new GUIStyle(GUI.skin.box)
+            {
+                padding = new RectOffset(10, 10, 10, 10)
+            };
+            
+            EditorGUILayout.Space(5);
+            EditorGUILayout.LabelField("훈련 데이터 설정", headerStyle);
+            EditorGUILayout.Space(5);
+            
+            EditorGUILayout.BeginVertical(boxStyle);
+            EditorGUILayout.LabelField("기본 정보", EditorStyles.boldLabel);
+            EditorGUILayout.Space(3);
+
+            EditorGUILayout.PropertyField(practiceStatType, new GUIContent("스탯 타입"));
+            EditorGUILayout.PropertyField(practiceStatName, new GUIContent("훈련 이름"));
+            
+            EditorGUILayout.Space(3);
+            EditorGUILayout.LabelField("설명", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+            personalpracticeDescription.stringValue = EditorGUILayout.TextArea(
+                personalpracticeDescription.stringValue, 
+                GUILayout.Height(60)
+            );
+            EditorGUI.indentLevel--;
+            
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.Space(5);
+            
+            EditorGUILayout.BeginVertical(boxStyle);
+            EditorGUILayout.LabelField("수치 설정", EditorStyles.boldLabel);
+            EditorGUILayout.Space(3);
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PropertyField(staminaReduction, new GUIContent("체력 소모"));
+            EditorGUILayout.LabelField($"(-{staminaReduction.floatValue})", GUILayout.Width(60));
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PropertyField(statIncrease, new GUIContent("스탯 증가량"));
+            EditorGUILayout.LabelField($"(+{statIncrease.floatValue})", GUILayout.Width(60));
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.Space(5);
+            
+            EditorGUILayout.BeginVertical(boxStyle);
+            showSuccessComment = EditorGUILayout.Foldout(showSuccessComment, "성공 코멘트", true, EditorStyles.foldoutHeader);
+            if (showSuccessComment)
+            {
+                EditorGUI.indentLevel++;
+                DrawCommentFields(personalsuccessComment);
+                EditorGUI.indentLevel--;
+            }
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.Space(5);
+            
+            EditorGUILayout.BeginVertical(boxStyle);
+            showFailComment = EditorGUILayout.Foldout(showFailComment, "실패 코멘트", true, EditorStyles.foldoutHeader);
+            if (showFailComment)
+            {
+                EditorGUI.indentLevel++;
+                DrawCommentFields(personalfaillComment);
+                EditorGUI.indentLevel--;
+            }
+            EditorGUILayout.EndVertical();
+            
+            EditorGUILayout.Space(10);
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            EditorGUILayout.LabelField("요약", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField($"훈련: {practiceStatName.stringValue}");
+            EditorGUILayout.LabelField($"타입: {practiceStatType.enumDisplayNames[practiceStatType.enumValueIndex]}");
+            EditorGUILayout.LabelField($"효과: 스탯 +{statIncrease.floatValue} / 체력 -{staminaReduction.floatValue}");
+            EditorGUILayout.EndVertical();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawCommentFields(SerializedProperty commentProp)
+        {
+            EditorGUILayout.PropertyField(commentProp.FindPropertyRelative("icon"), new GUIContent("아이콘"));
+            
+            EditorGUILayout.LabelField("대사", EditorStyles.boldLabel);
+            var commentText = commentProp.FindPropertyRelative("comment");
+            commentText.stringValue = EditorGUILayout.TextArea(commentText.stringValue, GUILayout.Height(40));
+
+            EditorGUILayout.Space(3);
+            EditorGUILayout.LabelField("생각", EditorStyles.boldLabel);
+            var thoughtsText = commentProp.FindPropertyRelative("thoughts");
+            thoughtsText.stringValue = EditorGUILayout.TextArea(thoughtsText.stringValue, GUILayout.Height(40));
+        }
+    }
+}
+#endif
---
+++ Assets/AddressableAssets/SO/Member/Practice/Editor/PracticeDataManagerWindow.cs
@@ -0,0 +1,292 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Code.MainSystem.StatSystem.BaseStats;
+using Code.MainSystem.StatSystem.Manager;
+
+namespace Code.MainSystem.MainScreen.MemberData
+{
+    public class PracticeDataManagerWindow : EditorWindow
+    {
+        private Vector2 scrollPosition;
+        private List<PersonalpracticeDataSO> allPracticeData;
+        private Dictionary<MemberType, List<PersonalpracticeDataSO>> memberPracticeDict;
+        private List<PersonalpracticeDataSO> teamPracticeData;
+        
+        private MemberType selectedMember = MemberType.Vocal;
+        private bool showTeamPractice = false;
+        private string searchFilter = "";
+        private PersonalpracticeDataSO editingPractice;
+
+        [MenuItem("Tools/Practice Data Manager")]
+        public static void ShowWindow()
+        {
+            GetWindow<PracticeDataManagerWindow>("훈련 데이터 매니저");
+        }
+
+        private void OnEnable()
+        {
+            LoadAllPracticeData();
+        }
+
+        private void LoadAllPracticeData()
+        {
+            string[] guids = AssetDatabase.FindAssets("t:PersonalpracticeDataSO");
+            allPracticeData = new List<PersonalpracticeDataSO>();
+
+            foreach (string guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                PersonalpracticeDataSO data = AssetDatabase.LoadAssetAtPath<PersonalpracticeDataSO>(path);
+                if (data != null)
+                    allPracticeData.Add(data);
+            }
+
+            OrganizeData();
+        }
+
+        private void OrganizeData()
+        {
+            memberPracticeDict = new Dictionary<MemberType, List<PersonalpracticeDataSO>>();
+            teamPracticeData = new List<PersonalpracticeDataSO>();
+
+            foreach (var data in allPracticeData)
+            {
+                if (data.name.Contains("Team") || data.name.Contains("팀"))
+                {
+                    teamPracticeData.Add(data);
+                }
+                else
+                {
+                    foreach (MemberType memberType in System.Enum.GetValues(typeof(MemberType)))
+                    {
+                        if (data.name.Contains(memberType.ToString()))
+                        {
+                            if (!memberPracticeDict.ContainsKey(memberType))
+                                memberPracticeDict[memberType] = new List<PersonalpracticeDataSO>();
+                            
+                            memberPracticeDict[memberType].Add(data);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.Space(10);
+            
+            GUIStyle headerStyle = new GUIStyle(GUI.skin.label)
+            {
+                fontSize = 16,
+                fontStyle = FontStyle.Bold,
+                alignment = TextAnchor.MiddleCenter
+            };
+            EditorGUILayout.LabelField("훈련 데이터 매니저", headerStyle);
+            EditorGUILayout.Space(5);
+
+            if (editingPractice != null)
+            {
+                DrawEditMode();
+                return;
+            }
+
+            EditorGUILayout.BeginHorizontal();
+            searchFilter = EditorGUILayout.TextField("검색", searchFilter);
+            if (GUILayout.Button("새로고침", GUILayout.Width(80)))
+            {
+                LoadAllPracticeData();
+            }
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space(5);
+
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Toggle(!showTeamPractice, "개인 훈련", "Button"))
+                showTeamPractice = false;
+            if (GUILayout.Toggle(showTeamPractice, "팀 훈련", "Button"))
+                showTeamPractice = true;
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space(10);
+
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+            if (!showTeamPractice)
+            {
+                DrawPersonalPractice();
+            }
+            else
+            {
+                DrawTeamPractice();
+            }
+
+            EditorGUILayout.EndScrollView();
+
+            EditorGUILayout.Space(10);
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            EditorGUILayout.LabelField("통계", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField($"전체 훈련 데이터: {allPracticeData.Count}개");
+            EditorGUILayout.LabelField($"개인 훈련: {allPracticeData.Count - teamPracticeData.Count}개");
+            EditorGUILayout.LabelField($"팀 훈련: {teamPracticeData.Count}개");
+            EditorGUILayout.EndVertical();
+        }
+
+        private void DrawEditMode()
+        {
+            EditorGUILayout.BeginHorizontal();
+            GUIStyle titleStyle = new GUIStyle(GUI.skin.label)
+            {
+                fontSize = 14,
+                fontStyle = FontStyle.Bold
+            };
+            EditorGUILayout.LabelField($"편집 중: {editingPractice.name}", titleStyle);
+            if (GUILayout.Button("돌아가기", GUILayout.Width(80)))
+            {
+                editingPractice = null;
+                GUI.FocusControl(null);
+            }
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space(10);
+
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+            SerializedObject so = new SerializedObject(editingPractice);
+            so.Update();
+
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            EditorGUILayout.LabelField("기본 정보", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(so.FindProperty("PracticeStatType"), new GUIContent("스탯 타입"));
+            EditorGUILayout.PropertyField(so.FindProperty("PracticeStatName"), new GUIContent("훈련 이름"));
+            EditorGUILayout.LabelField("설명");
+            var desc = so.FindProperty("PersonalpracticeDescription");
+            desc.stringValue = EditorGUILayout.TextArea(desc.stringValue, GUILayout.Height(60));
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.Space(5);
+
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            EditorGUILayout.LabelField("수치 설정", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(so.FindProperty("StaminaReduction"), new GUIContent("체력 소모"));
+            EditorGUILayout.PropertyField(so.FindProperty("statIncrease"), new GUIContent("스탯 증가량"));
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.Space(5);
+
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            EditorGUILayout.LabelField("성공 코멘트", EditorStyles.boldLabel);
+            DrawCommentProperty(so.FindProperty("PersonalsuccessComment"));
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.Space(5);
+
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            EditorGUILayout.LabelField("실패 코멘트", EditorStyles.boldLabel);
+            DrawCommentProperty(so.FindProperty("PersonalfaillComment"));
+            EditorGUILayout.EndVertical();
+
+            so.ApplyModifiedProperties();
+            
+            EditorGUILayout.EndScrollView();
+
+            EditorGUILayout.Space(10);
+            if (GUILayout.Button("저장", GUILayout.Height(30)))
+            {
+                EditorUtility.SetDirty(editingPractice);
+                AssetDatabase.SaveAssets();
+                EditorUtility.DisplayDialog("저장 완료", "변경사항이 저장되었습니다.", "확인");
+            }
+        }
+
+        private void DrawCommentProperty(SerializedProperty commentProp)
+        {
+            EditorGUILayout.PropertyField(commentProp.FindPropertyRelative("icon"), new GUIContent("아이콘"));
+            EditorGUILayout.LabelField("대사");
+            var comment = commentProp.FindPropertyRelative("comment");
+            comment.stringValue = EditorGUILayout.TextArea(comment.stringValue, GUILayout.Height(40));
+            EditorGUILayout.LabelField("생각");
+            var thoughts = commentProp.FindPropertyRelative("thoughts");
+            thoughts.stringValue = EditorGUILayout.TextArea(thoughts.stringValue, GUILayout.Height(40));
+        }
+
+        private void DrawPersonalPractice()
+        {
+            selectedMember = (MemberType)EditorGUILayout.EnumPopup("멤버 선택", selectedMember);
+            EditorGUILayout.Space(5);
+
+            if (!memberPracticeDict.ContainsKey(selectedMember))
+            {
+                EditorGUILayout.HelpBox($"{selectedMember}의 훈련 데이터가 없습니다.", MessageType.Info);
+                return;
+            }
+
+            var practices = memberPracticeDict[selectedMember];
+            var filteredPractices = string.IsNullOrEmpty(searchFilter) 
+                ? practices 
+                : practices.Where(p => p.name.ToLower().Contains(searchFilter.ToLower())).ToList();
+
+            EditorGUILayout.LabelField($"{selectedMember} 훈련 목록 ({filteredPractices.Count}개)", EditorStyles.boldLabel);
+            EditorGUILayout.Space(5);
+
+            foreach (var practice in filteredPractices)
+            {
+                DrawPracticeCard(practice);
+            }
+        }
+
+        private void DrawTeamPractice()
+        {
+            var filteredPractices = string.IsNullOrEmpty(searchFilter)
+                ? teamPracticeData
+                : teamPracticeData.Where(p => p.name.ToLower().Contains(searchFilter.ToLower())).ToList();
+
+            EditorGUILayout.LabelField($"팀 훈련 목록 ({filteredPractices.Count}개)", EditorStyles.boldLabel);
+            EditorGUILayout.Space(5);
+
+            foreach (var practice in filteredPractices)
+            {
+                DrawPracticeCard(practice);
+            }
+        }
+
+        private void DrawPracticeCard(PersonalpracticeDataSO practice)
+        {
+            if (practice == null) return;
+
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(practice.PracticeStatName, EditorStyles.boldLabel, GUILayout.Width(150));
+            EditorGUILayout.LabelField($"[{practice.PracticeStatType}]", GUILayout.Width(80));
+            
+            if (GUILayout.Button("선택", GUILayout.Width(50)))
+            {
+                Selection.activeObject = practice;
+                EditorGUIUtility.PingObject(practice);
+            }
+            
+            if (GUILayout.Button("편집", GUILayout.Width(50)))
+            {
+                editingPractice = practice;
+                GUI.FocusControl(null);
+            }
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.LabelField($"파일명: {practice.name}", EditorStyles.miniLabel);
+            EditorGUILayout.LabelField($"체력: -{practice.StaminaReduction} | 스탯: +{practice.statIncrease}", EditorStyles.miniLabel);
+            
+            if (!string.IsNullOrEmpty(practice.PersonalpracticeDescription))
+            {
+                EditorGUILayout.LabelField(practice.PersonalpracticeDescription, EditorStyles.wordWrappedMiniLabel);
+            }
+
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.Space(3);
+        }
+    }
+}
+#endif
