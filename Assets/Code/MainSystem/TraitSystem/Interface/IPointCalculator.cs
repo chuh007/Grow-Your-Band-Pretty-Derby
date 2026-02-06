78595e731e6986b5640df9a5e31e7da9c22758d0
kimjinho <russiakim2008@gmail.com>
2026-02-06 14:35:05 +0900

    feat : new trait effects

--- Assets/Code/MainSystem/TraitSystem/Interface/IPointCalculator.cs
+++ Assets/Code/MainSystem/TraitSystem/Interface/IPointCalculator.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using Code.MainSystem.TraitSystem.Data;
 using Code.MainSystem.TraitSystem.Runtime;
 
 namespace Code.MainSystem.TraitSystem.Interface
@@ -7,7 +6,5 @@ namespace Code.MainSystem.TraitSystem.Interface
     public interface IPointCalculator
     {
         int CalculateTotalPoints(IEnumerable<ActiveTrait> traits);
-        int CalculateAfterRemoval(ITraitHolder holder, ActiveTrait toRemove);
-        int CalculateAfterAdd(ITraitHolder holder, TraitDataSO toAdd);
     }
 }
