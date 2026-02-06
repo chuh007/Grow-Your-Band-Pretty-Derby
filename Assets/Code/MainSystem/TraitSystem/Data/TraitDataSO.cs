89a0718b8ef016a03ebb4b7efce90035298c3003
kimjinho <russiakim2008@gmail.com>
2026-02-06 13:41:36 +0900

    refactor : trait data system

--- Assets/Code/MainSystem/TraitSystem/Data/TraitDataSO.cs
+++ Assets/Code/MainSystem/TraitSystem/Data/TraitDataSO.cs
@@ -1,73 +1,42 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections.Generic;
-using Code.MainSystem.TraitSystem.TraitConditions;
 using Code.MainSystem.TraitSystem.TraitEffect;
 
 namespace Code.MainSystem.TraitSystem.Data
 {
-    /// <summary>
-    /// 어떨때 실행이 되는지
-    /// </summary>
-    public enum TraitEffectType
+    [Serializable]
+    public struct StatImpact
     {
-        OnTurnStart,
-        OnActionSelect,
-        OnActionExecute,
-        OnActionComplete,
-        OnTurnEnd,
-        Passive
+        public TraitTarget Target;      // N번째 효과가 적용될 대상
+        public CalculationType CalcType; // 계산 방식
+        public string RequiredTag;       // 특정 조건
     }
-
-    public enum TraitType
-    {
-        NoneTrait,              // 특성 없음
-        Telepathy,              // 이심전심
-        LoneGuitarist,          // 고독한 기타리스트
-        ShiningEyes,            // 반짝이는 눈
-        FailureBreedsSuccess,   // 실패는 성공의 어머니
-        HonedTechnique,         // 단련기술
-        Injury,                 // 부상
-        Overzealous,            // 지나친 열정
-        Dogmatic,               // 독선적
-        Entertainer,            // 만담가
-        Focus,                  // 집중력
-        HighlightBoost,         // 하이라이트 강화
-        AttentionGain,          // 주목도 상승
-        BreathControl,          // 호흡 조절
-    }
-
+    
     [CreateAssetMenu(fileName = "Trait data", menuName = "SO/Trait/Trait data")]
     public class TraitDataSO : ScriptableObject
     {
-        public int TraitID;
         public TraitType TraitType;
         public string TraitName;
         public Sprite TraitIcon;
-        public TraitEffectType traitEffectType;
         public int MaxLevel;
         public int Point;
         public bool IsRemovable = true;
+        public List<StatImpact> Impacts;
         public List<float> Effects = new();
+        
         [TextArea] public string DescriptionEffect;
         
-        [HideInInspector] public string conditionName;
-        [HideInInspector] public string effectName;
-        
-        public AbstractTraitCondition CreateConditionInstance()
-        {
-            if (string.IsNullOrEmpty(conditionName))
-                return null;
-            Type t = Type.GetType(conditionName);
-            return t != null ? Activator.CreateInstance(t) as AbstractTraitCondition : null;
-        }
+        [Header("Special Case")]
+        public string SpecialLogicClassName;
 
         public AbstractTraitEffect CreateEffectInstance()
         {
-            if (string.IsNullOrEmpty(effectName)) 
-                return null;
-            Type t = Type.GetType(effectName);
-            return t != null ? Activator.CreateInstance(t) as AbstractTraitEffect : null;
+            if (string.IsNullOrEmpty(SpecialLogicClassName))
+                return new MultiStatModifierEffect();
+
+            var type = System.Type.GetType(SpecialLogicClassName);
+            return (AbstractTraitEffect)System.Activator.CreateInstance(type);
         }
     }
 }
---
+++ Assets/Code/MainSystem/TraitSystem/Data/TraitTypes.cs
@@ -0,0 +1,40 @@
+﻿namespace Code.MainSystem.TraitSystem.Data
+{
+    public enum TraitType
+    {
+        NoneTrait,              // 특성 없음
+        Telepathy,              // 이심전심
+        LoneGuitarist,          // 고독한 기타리스트
+        ShiningEyes,            // 반짝이는 눈
+        FailureBreedsSuccess,   // 실패는 성공의 어머니
+        HonedTechnique,         // 단련기술
+        Injury,                 // 부상
+        Overzealous,            // 지나친 열정
+        Dogmatic,               // 독선적
+        Entertainer,            // 만담가
+        Focus,                  // 집중력
+        HighlightBoost,         // 하이라이트 강화
+        AttentionGain,          // 주목도 상승
+        BreathControl,          // 호흡 조절
+    }
+
+    public enum TraitTarget
+    {
+        Ensemble,       // 합주 효과 관련
+        Practice,       // 개인 연습 효과 관련
+        Condition,      // 컨디션 소모/변화 관련
+        SuccessRate,    // 훈련/합주 성공률 관련
+        Training,       // 능력치 상승 효율 관련
+        Mental,         // 멘탈 능력치 관련
+        FeverScore,     // 피버 점수 관련
+        FeverTime,      // 피버 지속시간 관련
+        FeverInput,     // 피버 필요 입력 관련
+    }
+
+    public enum CalculationType
+    {
+        Additive,           //+-
+        Multiplicative,     //X
+        Percentage          //%
+    }
+}
