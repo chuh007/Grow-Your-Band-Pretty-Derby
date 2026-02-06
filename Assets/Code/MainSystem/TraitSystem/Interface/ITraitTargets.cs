fdc659f28ef2fa614807638676dcdcbc5d32a163
kimjinho <russiakim2008@gmail.com>
2026-02-06 13:44:23 +0900

    refactor : trait interface

---
+++ Assets/Code/MainSystem/TraitSystem/Interface/ITraitTargets.cs
@@ -0,0 +1,35 @@
+﻿namespace Code.MainSystem.TraitSystem.Interface
+{
+    public interface ISuccessGuarantor
+    {
+        bool ShouldGuarantee();
+    }
+
+    public interface IInspirationSystem
+    {
+        void OnTrainingFailed();
+    }
+
+    public interface ITurnProcessListener
+    {
+        void OnTurnPassed();
+    }
+    
+    internal interface IJudgmentCorrection
+    {
+        bool CorrectMissToGood { get; }
+    }
+    
+    // TODO 연결 작업후 삭제
+    internal interface IFeverInputStat
+    { }
+
+    internal interface IFeverTimeStat
+    { }
+
+    public interface IFeverScoreStat
+    { }
+    
+    internal interface IMentalStat
+    { }
+}
