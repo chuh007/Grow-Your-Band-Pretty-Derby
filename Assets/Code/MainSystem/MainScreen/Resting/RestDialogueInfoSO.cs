1e5dccca3876d69fe2d6824a72e26b702abc5744
kimjinho <russiakim2008@gmail.com>
2026-02-08 20:58:25 +0900

    feat : rest select button

---
+++ Assets/Code/MainSystem/MainScreen/Resting/RestDialogueInfoSO.cs
@@ -0,0 +1,14 @@
+﻿using Code.MainSystem.Dialogue;
+using Code.MainSystem.StatSystem.Manager;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Code.MainSystem.MainScreen.Resting
+{
+    [CreateAssetMenu(fileName = "Rest dialogue", menuName = "SO/Rest/Rest dialogue info", order = 0)]
+    public class RestDialogueInfoSO : ScriptableObject
+    {
+        public MemberType MemberType;
+        public AssetReferenceT<DialogueInformationSO> RestInformation;
+    }
+}
